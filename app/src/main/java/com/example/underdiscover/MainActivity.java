4061c487fd4aa82f77aab989d3d7e152ddb842ac
MorganC46 <35434590+MorganC46@users.noreply.github.com>
2020-02-07 14:50:16 +0000

    Added Project

---
+++ app/src/main/java/com/example/underdiscover/MainActivity.java
@@ -0,0 +1,179 @@
+package com.example.underdiscover;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.spotify.protocol.client.CallResult;
+import com.spotify.protocol.types.Track;
+import com.spotify.sdk.android.authentication.*;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "5abdc4ff7e8347bebd727e823afa7df7";
+    private static final String REDIRECT_ID = "com.example.underdiscover://callback";
+    private static final int REQUEST_CODE = 1337;
+    private SpotifyAppRemote mSpotifyAppRemote;
+    private int playerState = 0;
+    private String ACCESS_TOKEN;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("UnderDiscover");
+
+        if (isNetworkAvailable() == false) {
+            Snackbar noInternet = Snackbar.make(findViewById(R.id.text), "No Network Detected", Snackbar.LENGTH_INDEFINITE);
+            noInternet.show();
+        }
+        else {
+            authenticateSpotify();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        ConnectionParams params = new ConnectionParams.Builder(CLIENT_ID)
+                .setRedirectUri(REDIRECT_ID).showAuthView(true).build();
+        SpotifyAppRemote.connect(this, params, new Connector.ConnectionListener() {
+            @Override
+            public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                mSpotifyAppRemote = spotifyAppRemote;
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                Log.e("MyActivity", throwable.getMessage(), throwable);
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSpotifyAppRemote.getPlayerApi().pause();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+
+    private void firstPlay(View view) {
+        getCurrentTrack(view);
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:0KJZaU4cVWKF68kyAoR1TA");
+    }
+
+    private void pause(View view) {
+        mSpotifyAppRemote.getPlayerApi().pause();
+    }
+
+    private void play(View view) {
+        getCurrentTrack(view);
+        mSpotifyAppRemote.getPlayerApi().resume();
+    }
+
+    private void getCurrentTrack(View view) {
+        mSpotifyAppRemote.getPlayerApi().subscribeToPlayerState().setEventCallback(playerState -> {
+            Track track = playerState.track;
+            if (track != null) {
+                TextView title = findViewById(R.id.currentlyPlaying);
+                title.setText("Currently Playing: " + track.name + " by " + track.artist.name);
+                ImageView image = findViewById(R.id.currentlyPlayingImage);
+                mSpotifyAppRemote.getImagesApi().getImage(track.imageUri).setResultCallback(new CallResult.ResultCallback<Bitmap>() {
+                    @Override
+                    public void onResult(Bitmap bitmap) {
+                        image.setImageBitmap(bitmap);
+                    }
+                });
+            }
+        });
+    }
+
+    private boolean isNetworkAvailable() {
+        //Boolean method to check for a network connection - used to make pathway decisions
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connManager != null) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+
+                NetworkCapabilities capabilities = connManager.getNetworkCapabilities(connManager.getActiveNetwork());
+                if (capabilities != null) {
+                    if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                        return true;
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                        return true;
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            } else {
+                NetworkInfo activeNetworkInfo = connManager.getActiveNetworkInfo();
+                return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+            }
+        }
+        return false;
+    }
+
+    private void authenticateSpotify() {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_ID);
+        builder.setScopes(new String[]{"user-read-playback-state,streaming,playlist-read-collaborative,user-modify-playback-state,playlist-modify-public,user-library-modify,user-top-read,user-read-currently-playing,playlist-read-private,app-remote-control,playlist-modify-private,user-library-read"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        ACCESS_TOKEN = AuthenticationClient.getResponse(resultCode, intent).getAccessToken();
+        Log.d("checkvalue", ACCESS_TOKEN);
+    }
+
+    public void onClickChangePlayerState(View view) {
+
+        Button changePlayerState = findViewById(R.id.changePlayerState);
+
+        switch (playerState) {
+            case 0:
+                firstPlay(view); playerState = 1;
+                changePlayerState.setText("Pause");
+                break;
+            case 1:
+                pause(view); playerState = 2;
+                changePlayerState.setText("Resume");
+                break;
+            case 2:
+                play(view); playerState = 1;
+                changePlayerState.setText("Pause");
+                break;
+        }
+    }
+
+    public void onClickSkipSong(View view) {
+        mSpotifyAppRemote.getPlayerApi().skipNext();
+    }
+
+    public void onClickShowMetaData(View view) {
+        mSpotifyAppRemote.getPlayerApi().subscribeToPlayerState().setEventCallback(playerState -> {
+            Intent metaDataIntent = new Intent(MainActivity.this, MetadataActivity.class);
+            metaDataIntent.putExtra("TrackID", playerState.track.uri);
+            MainActivity.this.startActivity(metaDataIntent);
+                });
+    }
+}
---
+++ app/src/main/java/com/example/underdiscover/MetadataActivity.java
@@ -0,0 +1,105 @@
+package com.example.underdiscover;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MetadataActivity extends AppCompatActivity {
+
+    private AsyncTask fetchInstance;
+    private final String METADATA_URL = "https://api.spotify.com/v1/audio-features/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metadata);
+
+//        String apiUrl = METADATA_URL +
+//
+//        fetchInstance = new HttpStuff.execute(apiUrl);
+    }
+
+    public void onClickReturnToMain(View view) {
+        finish();
+    }
+
+    private class HttpStuff extends AsyncTask<String, Void, String> {
+        //Connection method
+        protected String doInBackground(String... urlString) {
+
+            String result = "";
+            HttpURLConnection urlConn;
+
+            try {
+                URL url = new URL(urlString[0]);
+                urlConn = (HttpURLConnection) url.openConnection();
+                urlConn.setRequestMethod("GET");
+
+                if (urlConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    result = streamIntoString(urlConn.getInputStream());
+                    dealWithResult(result);
+                }
+            }
+
+            catch(MalformedURLException eURL) {
+                eURL.printStackTrace();
+                System.exit(1);
+            }
+
+            catch(IOException eIO) {
+                eIO.printStackTrace();
+                System.exit(2);
+            }
+
+            return null;
+        }
+    }
+
+    protected String streamIntoString(InputStream stream) {
+        //Method to process and correctly separate input streams
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            String data;
+            String result = "";
+
+            while ((data = reader.readLine()) != null) {
+                result += data;
+            }
+            if (null != stream) {
+                stream.close();
+            }
+            return result;
+        }
+        catch (IOException eIO) {
+            eIO.printStackTrace();
+            System.exit(3);
+        }
+        return null;
+    }
+
+    protected void dealWithResult(String result) {
+        Log.d("RESULT", result);
+//        try {
+//            JSONObject jsonResp = new JSONObject(result);
+//        }
+//        catch(JSONException eJSON) {
+//            eJSON.printStackTrace();
+//            System.exit(3);
+//        }
+    }
+}
