f9a92454b967717b81d7ae49456b166ffb76c659
MorganC46 <morgancentini38@gmail.com>
2020-02-13 17:08:14 +0000

    Popup box for Metadata /minor/

--- app/src/main/java/com/example/underdiscover/MetaDataListAdapter.java
+++ app/src/main/java/com/example/underdiscover/MetaDataListAdapter.java
@@ -1,10 +1,15 @@
 package com.example.underdiscover;
 
 import android.app.Activity;
+import android.content.Context;
+import android.text.Layout;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class MetaDataListAdapter extends ArrayAdapter {
@@ -27,6 +32,23 @@ public class MetaDataListAdapter extends ArrayAdapter {
 
         TextView attributeField = rowView.findViewById(R.id.attributeText);
         attributeField.setText(metadata[count]);
+
+        attributeField.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater popupInflater = (LayoutInflater)
+                        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View popupView = popupInflater.inflate(R.layout.popup_window_metadata, null);
+
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
+
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                //TODO: Decide whether to implement onTouch to dismiss if touched on box, not just outside of it
+            }
+        });
 
         return rowView;
     }
