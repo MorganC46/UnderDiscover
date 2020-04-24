e32560053a7574c4857601915acb70df577c8a29
MorganC46 <35434590+MorganC46@users.noreply.github.com>
2020-04-24 02:12:33 +0100

    V1.2
    Fixed Proguard rules meaning app now works independently on mobile!
    
    Fixed Media Player Bug (created a new one but much more minor)

--- app/src/main/java/com/example/underdiscover/AudioPreviewManager.java
+++ app/src/main/java/com/example/underdiscover/AudioPreviewManager.java
@@ -1,9 +1,13 @@
 package com.example.underdiscover;
 
 import android.content.Context;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
 
 // CODE INSPIRED BY "veeson" ON STACK OVERFLOW
 // https://stackoverflow.com/questions/8486147/how-can-i-play-a-mp3-without-download-from-the-url
@@ -16,31 +20,37 @@ public class AudioPreviewManager {
         if (mediaPlayer != null) {
             killMediaPlayer();
         }
+        mediaPlayer = new MediaPlayer();
 
-        if (mediaPlayer == null) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            mediaPlayer.setAudioAttributes(
+                    new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .build()
+            );
+        } else {
             mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-            mediaPlayer = MediaPlayer.create(context, Uri.parse(url));
-            mediaPlayer.prepare();
         }
 
+        mediaPlayer.setDataSource(context, Uri.parse(url));
+        mediaPlayer.prepare();
+        mediaPlayer.start();
         mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-
             @Override
             public void onCompletion(MediaPlayer mp) {
                 killMediaPlayer();
             }
         });
-        mediaPlayer.start();
     }
 
     protected static void killMediaPlayer() {
-        if (mediaPlayer != null) {
-            try {
-                mediaPlayer.reset();
-                mediaPlayer.release();
-                mediaPlayer = null;
-            } catch (Exception e) {
-            }
+        try {
+            mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        } catch (Exception e) {
         }
     }
 
